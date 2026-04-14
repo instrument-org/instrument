bbfdb74a9351192ec59381582201e1745351934d
Jeremy Mack <mutewinter@users.noreply.github.com>
2026-04-14 16:27:14 -0500

    chore(studio): remove some old branding

--- apps/studio/src/client/routes/settings/advanced.tsx
+++ apps/studio/src/client/routes/settings/advanced.tsx
@@ -2,7 +2,7 @@ import { ExternalLink } from "@/client/components/external-link";
 import { Label } from "@/client/components/ui/label";
 import { Switch } from "@/client/components/ui/switch";
 import { rpcClient } from "@/client/rpc/client";
-import { APP_REPO_URL } from "@instrument-org/shared";
+import { APP_NAME, APP_REPO_URL } from "@instrument-org/shared";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import { toast } from "sonner";
@@ -59,7 +59,7 @@ function UsageMetrics() {
               onCheckedChange={handleToggleUsageMetrics}
             />
             <Label className="inline" htmlFor="usage-metrics">
-              Help Quests improve by submitting anonymous{" "}
+              Help {APP_NAME} improve by submitting{" "}
               <ExternalLink
                 className="text-blue-600 hover:underline dark:text-blue-400"
                 href={`${APP_REPO_URL}/blob/main/docs/usage-metrics.md`}

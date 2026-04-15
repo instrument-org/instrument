d9bbcf52a29a2752d76a3b20faa084625fb44eb4
Jeremy Mack <mutewinter@users.noreply.github.com>
2026-04-15 10:38:18 -0500

    fix(studio): incorrect updates folder

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -2,6 +2,7 @@ export const AI_GATEWAY_API_PATH = "/ai-gateway";
 export const APP_NAME = "Instrument";
 const APP_NAME_LOWER = "instrument";
 export const APP_NAME_SLUG = APP_NAME_LOWER.replaceAll(" ", "-");
+export const APP_UPDATER_CACHE_DIR_NAME = `${APP_NAME_SLUG}-desktop-updater`;
 export const APP_PROTOCOL =
   process.env.NODE_ENV === "development"
     ? `${APP_NAME_LOWER}-local`

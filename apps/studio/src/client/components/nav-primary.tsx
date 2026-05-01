d23c582f848eca0bb0d516c468c17706f458bbd4
Jeremy Mack <mutewinter@users.noreply.github.com>
2026-05-01 12:17:08 -0500

    feat(studio): brand styled new button

--- apps/studio/src/client/components/nav-primary.tsx
+++ apps/studio/src/client/components/nav-primary.tsx
@@ -7,6 +7,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/client/components/ui/sidebar";
+import { cn } from "@/client/lib/utils";
 import { type Icon } from "@phosphor-icons/react";
 import { type LinkProps } from "@tanstack/react-router";
 import React from "react";
@@ -19,27 +20,34 @@ export function NavPrimary({
     badge?: React.ReactNode;
     icon: Icon;
     isActive?: boolean;
+    isBrand?: boolean;
     isWarning?: boolean;
     title: string;
     url: LinkProps["to"];
   }[];
 }) {
   return (
-    <SidebarGroup {...props} className="pl-1">
+    <SidebarGroup {...props} className="pb-1 pl-1">
       <SidebarGroupContent>
         <SidebarMenu>
           {items.map((item) => (
             <SidebarMenuItem className="group" key={item.title}>
               <SidebarMenuButton
                 asChild
-                className="group-hover:bg-black/10 data-[active=true]:bg-black/15 data-[active=true]:font-normal data-[active=true]:text-foreground dark:group-hover:bg-white/10 dark:data-[active=true]:bg-white/15"
-                isActive={item.isActive}
+                className={cn(
+                  "group-hover:bg-black/10 data-[active=true]:bg-black/15 data-[active=true]:font-normal data-[active=true]:text-foreground dark:group-hover:bg-white/10 dark:data-[active=true]:bg-white/15",
+                  item.isBrand &&
+                    "font-bold data-[active=true]:bg-transparent data-[active=true]:text-brand",
+                )}
+                isActive={item.isBrand ? false : item.isActive}
               >
                 <InternalLink
                   className={
                     item.isWarning
                       ? "text-warning-foreground [&>svg]:size-4 [&>svg]:text-warning-foreground"
-                      : "[&>svg]:size-4"
+                      : item.isBrand
+                        ? "font-bold text-brand [&>svg]:size-4 [&>svg]:text-brand"
+                        : "[&>svg]:size-4"
                   }
                   openInCurrentTab
                   to={item.url}

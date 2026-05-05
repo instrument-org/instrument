58a1950db8724ac3cc77e847f1bb9205dfe2da33
Jeremy Mack <mutewinter@users.noreply.github.com>
2026-05-05 15:10:39 -0500

    chore(studio): fix lint

--- apps/studio/src/client/routes/_app/debug/components/index.tsx
+++ apps/studio/src/client/routes/_app/debug/components/index.tsx
@@ -6,6 +6,7 @@ export const Route = createFileRoute("/_app/debug/components/")({
   beforeLoad: () => {
     const defaultSessionId = presetSessions[0]?.id;
 
+    // eslint-disable-next-line @typescript-eslint/only-throw-error
     throw redirect({
       search: defaultSessionId ? { session: defaultSessionId } : undefined,
       to: "/debug/components/session-stream",

58836a907e3ac7e828074542882ba955e8fdf9db
Neil Renicker <neilrenicker@gmail.com>
2026-05-06 19:11:15 -0500

    feat(studio): tighten vertical spacing in sidebar content

--- apps/studio/src/client/components/ui/sidebar.tsx
+++ apps/studio/src/client/components/ui/sidebar.tsx
@@ -103,7 +103,7 @@ function SidebarContent({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       className={cn(
-        "flex min-h-0 flex-1 flex-col gap-5 overflow-auto group-data-[collapsible=icon]:overflow-hidden",
+        "flex min-h-0 flex-1 flex-col gap-1 overflow-auto group-data-[collapsible=icon]:overflow-hidden",
         className,
       )}
       data-sidebar="content"

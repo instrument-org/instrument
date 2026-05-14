33abe4efea5a61c6810318875c8ac0d3876cfb0f
Jeremy Mack <mutewinter@users.noreply.github.com>
2026-05-14 16:50:05 -0500

    chore: disable adding referral link for beta

--- packages/shared/src/add-ref.ts
+++ packages/shared/src/add-ref.ts
@@ -1,14 +1,16 @@
-import { APP_DOMAIN } from "./constants";
+// import { APP_DOMAIN } from "./constants";
 
 export const REF_PARAM_KEY = "ref";
 
 export function addRef(url: string): string {
-  try {
-    const urlObj = new URL(url);
-    urlObj.searchParams.set(REF_PARAM_KEY, APP_DOMAIN);
-    return urlObj.toString();
-  } catch {
-    const separator = url.includes("?") ? "&" : "?";
-    return `${url}${separator}${REF_PARAM_KEY}=${APP_DOMAIN}`;
-  }
+  // Disabled on 2026-05-14 pending FP-1028
+  return url;
+  // try {
+  //   const urlObj = new URL(url);
+  //   urlObj.searchParams.set(REF_PARAM_KEY, APP_DOMAIN);
+  //   return urlObj.toString();
+  // } catch {
+  //   const separator = url.includes("?") ? "&" : "?";
+  //   return `${url}${separator}${REF_PARAM_KEY}=${APP_DOMAIN}`;
+  // }
 }

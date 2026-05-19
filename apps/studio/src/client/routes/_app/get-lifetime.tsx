394d39c02935f0900afa65ab5d69f699b0e1dbe6
Jeremy Mack <mutewinter@users.noreply.github.com>
2026-05-19 14:32:27 -0500

    fix(studio): don't show upgrade if not possible

--- apps/studio/src/client/routes/_app/get-lifetime.tsx
+++ apps/studio/src/client/routes/_app/get-lifetime.tsx
@@ -1,8 +1,15 @@
+import { rpcClient } from "@/client/rpc/client";
+import { safe } from "@orpc/client";
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_app/get-lifetime")({
-  beforeLoad: () => {
+  beforeLoad: async () => {
+    const { data: hasToken } = await safe(rpcClient.auth.hasToken.call());
     // eslint-disable-next-line @typescript-eslint/only-throw-error
-    throw redirect({ search: { dialog: "lifetime" }, to: "/new-tab" });
+    throw redirect({
+      // If they aren't logged in, show the login dialog instead.
+      search: { dialog: hasToken ? "lifetime" : "login" },
+      to: "/new-tab",
+    });
   },
 });

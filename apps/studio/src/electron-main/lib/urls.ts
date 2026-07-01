f4bd536baab73150bbf2c5126ebc5e953212ecc6
Jeremy Mack <mutewinter@users.noreply.github.com>
2026-07-01 15:11:51 -0500

    studio: clean up lingering lints

--- apps/studio/src/electron-main/lib/urls.ts
+++ apps/studio/src/electron-main/lib/urls.ts
@@ -7,11 +7,6 @@ export function studioURL(pathname: StudioPath) {
   return createBaseURL("../renderer/index.html", pathname);
 }
 
-// Unsafe because it's a direct cast
-export function unsafe_studioURL(pathname: string) {
-  return studioURL(pathname as StudioPath);
-}
-
 function createBaseURL(rendererPath: string, hash = "") {
   if (is.dev && process.env.ELECTRON_RENDERER_URL) {
     const baseUrl = new URL(process.env.ELECTRON_RENDERER_URL);

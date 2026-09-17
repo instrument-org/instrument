39cbe8ab607058bd690daadf9e4855d977a2a8db
Jeremy Mack <mutewinter@users.noreply.github.com>
2026-09-17 17:15:36 -0500

    workspace: drop the eval case for a task's show command, which a task no longer has

--- packages/workspace/evals/cases/index.ts
+++ packages/workspace/evals/cases/index.ts
@@ -10,7 +10,6 @@ import { PDF_SKILL_EVALS } from "./pdf-skill";
 import { PROJECT_FOLDER_DISCIPLINE_EVALS } from "./project-folder-discipline";
 import { PROJECT_INSTRUCTIONS_EVALS } from "./project-instructions";
 import { SANDBOXED_PYTHON_EVALS } from "./sandboxed-python";
-import { SHOW_EVALS } from "./show";
 import { SOURCE_LINKS_EVALS } from "./source-links";
 import { UNREADABLE_MEDIA_EVALS } from "./unreadable-media";
 import { WEB_SEARCH_EVALS } from "./web-search";
@@ -29,7 +28,6 @@ export const EVALS = [
   ...PROJECT_FOLDER_DISCIPLINE_EVALS,
   ...PROJECT_INSTRUCTIONS_EVALS,
   ...SANDBOXED_PYTHON_EVALS,
-  ...SHOW_EVALS,
   ...SOURCE_LINKS_EVALS,
   ...UNREADABLE_MEDIA_EVALS,
   ...WEB_SEARCH_EVALS,
